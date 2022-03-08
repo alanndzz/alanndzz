eb2583957571a6c7f1c67ce801d4a025ae0bf226
alanndz <alanndz7@gmail.com>
2022-03-08 07:56:44 +0000

    Add bot task committer
    Co-authored-by: sutanlab <contact@gading.dev>
    Signed-off-by: alanndz <alanndz7@gmail.com>

---
+++ task.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+arr[0]="bot: 👋 Hello Github!"
+arr[1]="bot: 🥳 Yeayyy!"
+arr[2]="bot: 😬 Working from github."
+arr[3]="bot: 👨‍💻 Work, work, work!"
+arr[4]="bot: 😪 Hufft..."
+arr[5]="bot: 😎 I'm working for my master!"
+arr[6]="bot: 🙄 Running task, again."
+arr[7]="bot: 👻 Thanks master."
+
+rand=$[$RANDOM % ${#arr[@]}]
+d=`date '+%Y-%m-%dT%H:%M:%SZ'`
+
+echo "$(cat base)" > README.md
+
+echo -e "\n## LAST UPDATED AT: ${d}" >> README.md
+
+git config --local user.email "alanskydream@gmail.com"
+git config --local user.name "alanndzz"
+git commit -am "${arr[$rand]} (at ${d})"
